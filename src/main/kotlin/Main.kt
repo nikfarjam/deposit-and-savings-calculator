b02e5b63f8befd41c631df7e5ad0f21bb35ab6b7
nikfarjam <mm.nikfarjam@gmail.com>
2024-02-13 15:24:21 +1100

    Impelement reporter

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,24 @@
 package com.mehdi
 
+import com.mehdi.model.Investment
+import com.mehdi.model.PaymentType
+import com.mehdi.service.ConsoleReportGenerator
+import com.mehdi.service.DepositTermCalculator
+import com.mehdi.service.ReportGenerator
+import com.mehdi.service.Reporter
 import org.slf4j.LoggerFactory
 
 private val logger = LoggerFactory.getLogger("Main")
 
 fun main() {
     logger.info("App started")
+
+    val calculator = createCalculator("depositTerm")
+    val saving = calculator.calculate(Investment(10000.0, 0.011, 36, PaymentType.Monthly))
+    val reporter = createReporter()
+    reporter.showResult(saving)
 }
+
+private fun createReporter() = Reporter(listOf<ReportGenerator>(ConsoleReportGenerator()))
+
+private fun createCalculator(type: String) = DepositTermCalculator()
---
+++ src/main/kotlin/service/Reporter.kt
@@ -0,0 +1,25 @@
+package com.mehdi.service
+
+import com.mehdi.error.CalculatorException
+import com.mehdi.model.Saving
+
+class Reporter(private val generators: List<ReportGenerator>) {
+
+    fun showResult(saving: Saving): List<String> {
+        val messages = mutableListOf<String>()
+
+        for(reportGenerator in generators) {
+            try {
+                reportGenerator.init()
+                reportGenerator.writeHeader(saving)
+                reportGenerator.writeBody(saving)
+                reportGenerator.writeFooter(saving)
+                messages.add("Successful ${reportGenerator.message()}")
+            } catch (ex: CalculatorException) {
+                messages.add(ex.message.toString())
+            }
+        }
+
+        return messages
+    }
+}
